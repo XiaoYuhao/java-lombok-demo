9b638d773ac02c5c274a58a1d321fd22be27fc93
Yuhao Xiao <t-yuhaoxiao@microsoft.com>
2022-07-08 10:43:39 +0800

    update

--- src/main/java/com/example/demo/student/student.java
+++ src/main/java/com/example/demo/student/student.java
@@ -3,8 +3,39 @@ package com.example.demo.student;
 import lombok.Data;
 
 @Data
-public class student {
-    private int id;
+public class student{
     private String name;
     private int age;
+    private int id;
+    private int score;
+}
+
+class Person {
+    private int id;
+    private int age;
+    private String name;
+    public Person() {
+    }
+}
+
+class Base1 {
+    private int v1;
+    private int v2;
+    public Base1(int v1, int v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+    public Base1(int v1) {
+        this.v1 = v1;
+    }
+}
+
+class Base2 extends Base1 {
+    private int v3;
+    private int v4;
+    public Base2(int v1, int v2, int v3, int v4) {
+        super(v1, v2);
+        this.v3 = v3;
+        this.v4 = v4;
+    }
 }
